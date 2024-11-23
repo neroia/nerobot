5fb64716170baf1d811bf3e88978d1f1d6e6490b
neroia <neroia.suporte@gmail.com>
2024-11-23 14:54:32 -0300

    teste  2
    te

---
+++ js/script.js
@@ -0,0 +1,23 @@
+
+
+        // Função para abrir e fechar a lista
+function abri() {
+    const lista = document.getElementById('beneficios');
+    if (lista.style.display === 'none' || lista.style.display === '') {
+        lista.style.display = 'block';  // Torna visível
+    } else {
+        lista.style.display = 'none';  // Torna invisível
+    }
+}
+
+function instagram() {
+    // Redireciona para o perfil do Instagram
+    window.open("https://www.instagram.com/neroia89/" ,  "_blank")
+  }
+  function tiktok(){
+    alert("em breve")
+  }
+  
+
+
+
